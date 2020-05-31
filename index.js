7e79ad94efa5da8df0e651848ec8ac19f07a5bd0
rutaks <rootsum.dev@gmail.com>
2020-05-31 07:40:23 +0200

    Refactored file function location

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
 
+const files = require("./lib/files");
+
 clear();
 
 console.log(
-  chalk.yellow(figlet.textSync("GNOR 😴", { horizontalLayout: "full" }))
+  chalk.yellow(figlet.textSync("GNOR", { horizontalLayout: "full" }))
 );
+
+if (files.directoryExists(".git")) {
+  console.log(chalk.red("A Git Repo Already Exists!"));
+  process.exit();
+}
