2acf58103d3c81980658c9f6d7b3d5a2345e414c
rutaks <rootsum.dev@gmail.com>
2020-05-31 13:39:17 +0200

    Added CLI endpoint

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
+#!/usr/bin/env node
 const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
 
 const files = require("./lib/files");
+const github = require("./lib/github");
+const repo = require("./lib/repo");
 
 clear();
 
-console.log(
-  chalk.yellow(figlet.textSync("GNOR", { horizontalLayout: "full" }))
-);
+console.log(chalk.green(figlet.textSync("GNOR", { horizontalLayout: "full" })));
 
-if (files.directoryExists(".git")) {
-  console.log(chalk.red("A Git Repo Already Exists!"));
-  process.exit();
-}
+const getGithubToken = async () => {
+  let token = github.getStoredAccessToken();
+  if (token) return token;
+  token = github.getPersonalAccessToken();
+  return token;
+};
+
+const run = async () => {
+  try {
+    const token = await getGithubToken();
+    github.githubAuth(token);
+    const url = await repo.createRemoteRepo();
+    await repo.createGitignore();
+    await repo.setupRepo(url);
+    console.log(chalk.green("You're all set :)"));
+  } catch (error) {
+    if (!error) {
+      console.log(chalk.red("Could not set up repo :("));
+      return;
+    }
+    switch (error.status) {
+      case 401:
+        console.log(
+          chalk.red("Could not log you in. Provide correct credentials")
+        );
+        break;
+      case 422:
+        console.log(chalk.red("A repo with the same already exists"));
+      default:
+        console.log(chalk.red(error));
+        break;
+    }
+  }
+};
+
+run();
